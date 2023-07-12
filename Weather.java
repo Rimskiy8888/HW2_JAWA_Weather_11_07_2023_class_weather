95b8b2fcb239e9e1d92640594920bfc2d4953911
Rimskiy8888 <137941314+Rimskiy8888@users.noreply.github.com>
2023-07-12 18:55:07 +0200

    Class_Weather_to_HW2_Jawa
    Class_Weather to HW2_Jawa

---
+++ Weather.java
@@ -0,0 +1,11 @@
+public class Weather {
+    String dailyWeather = "DAILY WEATHER";
+    String nameOfTheDay = "Day";
+    byte tempOfTheAirMin = 12;
+    byte tempOfTheAirMax = 27;
+    boolean isItRain = false;
+    boolean isItSnow = false;
+    double windSpeed = 37.9;
+    int humidity = 5;
+
+}
